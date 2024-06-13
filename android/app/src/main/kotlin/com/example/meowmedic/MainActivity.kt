9a3d8aa8118eb0ad753e25ee8bc709ddcd36b620
aarsyah0 <arsasaja2@gmail.com>
2024-06-13 13:16:00 +0700

    First Upload

---
+++ android/app/src/main/kotlin/com/example/meowmedic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meowmedic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:meowmedic/signupscreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Get.off(() => SignUpScreen());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFB7E5E),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'meow medic',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 9),
+            Image.asset(
+              'assets/catgroup.png',
+              height: 100,
+              width: 300,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
